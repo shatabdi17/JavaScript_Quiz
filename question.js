d8d1873a3981f8e0f58378fc21f4ada7fa66f2a8
Shatabdi Bhattacharjee <shatabdi17@gmail.com>
2018-05-03 17:09:27 -0400

    made changed to all files

--- question.js
+++ question.js
@@ -1,68 +1,68 @@
 let questions = [{
-    "question": "Which of these is not a logical operator?",
+    "question": "1. Which of these is not a logical operator?",
     "option1": "&&",
     "option2": "!",
     "option3": "!!",
     "option4": "||",
     "answer": "!!"
 }, {
-    "question": "Which of the following function of String object combines the text of two strings and returns a new string?",
+    "question": "2. Which of the following function of String object combines the text of two strings and returns a new string?",
     "option1": "add()",
     "option2": "merge()",
     "option3": "concat()",
     "option4": "append()",
     "answer": "concat()"
 }, {
-    "question": "Which of the following methods does not return a new array?",
+    "question": "3. Which of the following methods does not return a new array?",
     "option1": "forEach()",
     "option2": "map()",
     "option3": "filer()",
     "option4": "every()",
     "answer": "forEach()"
 }, {
-    "question": "Which is not a primitive data type in JavaScript?",
+    "question": "4. Which is not a primitive data type in JavaScript?",
     "option1": "Boolean",
     "option2": "Null",
     "option3": "Character",
     "option4": "Undefined",
     "answer": "Character"
 }, {
-    "question": "What character ends a javascript statement?",
+    "question": "5. What character ends a javascript statement?",
     "option1": ":",
     "option2": ".",
     "option3": ";",
     "option4": ",",
     "answer": ";"
 }, {
-    "question": "Is JavaScript case sensitive?",
+    "question": "6. Is JavaScript case sensitive?",
     "option1": "No",
     "option2": "Yes",
     "option3": "May be?",
     "option4": "Not sure!",
     "answer": "Yes"
 }, {
-    "question": "What does 3+4+'7' evaluate to?",
+    "question": "7. What does 3+4+'7' evaluate to?",
     "option1": "14",
     "option2": "7",
     "option3": "77",
     "option4": "error",
     "answer": "77"
 }, {
-    "question": "What looping structures are there in JavaScript?",
+    "question": "8. What looping structures are there in JavaScript?",
     "option1": "for",
     "option2": "while",
     "option3": "do-while",
     "option4": "all of them",
     "answer": "all of them"
 }, {
-    "question": "If button is clicked which event handler is invoked?",
+    "question": "9. If button is clicked which event handler is invoked?",
     "option1": "onsubmit",
     "option2": "onclick",
     "option3": "onload",
     "option4": "ispostback",
     "answer": "onsubmit"
 }, {
-    "question": "What is the function of array object that adds and or removes elements from an array?",
+    "question": "10. What is the function of array object that adds and or removes elements from an array?",
     "option1": "sort()",
     "option2": "splice()",
     "option3": "unsplice()",
--- script.js
+++ script.js
@@ -14,59 +14,105 @@ $(function () {
     const opt3 = $('#opt3');
     const opt4 = $('#opt4');
     const previousButton = $('#prevButton');
-    const nextButton = $('nextButton');
+    const nextButton = $('#nextButton');
     const resultContainer = $('#result');
+    let q;
 
+// PAGE LOAD 
+
+setTimeout(function () {
+    $('.footer-container').hide();
+}, 1000);
+
+$('.main-container').hide();
+$('.start').on('click', function(){
+    $(".quiz-start-page").hide();
+    $('.main-container').show();
+
+});
 // CREATE A FUNCTION TO LOAD THE QUESTION
-
   const loadQuestion = ((index) => {
-    let q = questions[index];
-    //console.log(q);
+    q = questions[index];
     questionQ.text(q.question);
+    opt1.siblings('input')[0].checked = false;
     opt1.text(q.option1);
+    opt2.siblings('input')[0].checked = false;
     opt2.text(q.option2);
+    opt3.siblings('input')[0].checked = false;
     opt3.text(q.option3);
+    opt4.siblings('input')[0].checked = false;
     opt4.text(q.option4);
  });
 
  loadQuestion(currentQuestion);
 // CREATE A FUNCTION TO LOAD THE NEXT QUESTION
+nextButton.on('click', function () {
+    currentQuestion = currentQuestion + 1;
+    loadNextQuestion();
+ 
+});
+previousButton.on('click', function () {
+    currentQuestion = currentQuestion - 1;
+    loadNextQuestion();
+  
+});
 
-const loadNextQuestion = (() => {
-    const selectedOption = $('input[type = radio]:checked');
-    if (!selectedOption) {
+function loadNextQuestion() {
+    const selectedOption = $('input[name=option]:checked');
+    if (selectedOption.length === 0) {
          alert('Please select your answer!');
           return;
      }
-    let answer = selectedOption.text() ;
+    let answer = selectedOption.siblings('span').text();
+    //console.log(answer);
     if (q.answer === answer) {
         score = score + 10;
     } else {
         score = score - 5;
     }
-    nextButton.on('click', function(){
-        loadNextQuestion();
-        currentQuestion = currentQuestion + 1;
-        //loadNextQuestion(currentQuestion++);
-    });
-    previousButton.on('click', function (){
-        currentQuestion = currentQuestion -1;
-        loadNextQuestion();
-        //loadNextQuestion(currentQuestion--);
-    });
+    console.log(score)
 
     if (currentQuestion == totalQuestions - 1) {
            nextButton.text('Finish');
       }
 
-
     if (currentQuestion == totalQuestions) {
-        container.css("display", none);
-        resultContainer.css("display", '');
-        resultContainer.text(`Your score is ${score}`); 
-           return;
+        container.css("display", "none");
+        resultContainer.css("display", "block");
+
+        //score description starts
+        if(score < 50) {
+            resultContainer.text(`Your score is ${score}! Uh oh you need work harder!`);
+        } else if (score >= 50 && score < 80) {
+            resultContainer.text(`Your score is ${score}! Wow, that\'s impressive!`);
+        } else if (score >= 80) {
+            resultContainer.text(`Your score is ${score}! You\'re a JS ninja! Want to take the next level?`);
+        }       
+         //score description ends
+        // resultContainer.text(`Your score is ${score}!`); 
+        return;
        }
-});
+
+    loadQuestion(currentQuestion);
+};
 
 
 });
+
+//  < !--TWITTER WIDGET -->
+window.twttr = (function (d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0],
+        t = window.twttr || {};
+    if (d.getElementById(id)) return t;
+    js = d.createElement(s);
+    js.id = id;
+    js.src = "https://platform.twitter.com/widgets.js";
+    fjs.parentNode.insertBefore(js, fjs);
+
+    t._e = [];
+    t.ready = function (f) {
+        t._e.push(f);
+    };
+    return t;
+}(document, "script", "twitter-wjs"));
+
